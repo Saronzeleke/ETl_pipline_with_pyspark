cb4a94911abae2376245eaec83f1a0238ee172ca
Saronzeleke <Cassiopiasaron@gmail.com>
2025-12-17 14:09:56 +0300

    dbt_models

--- nyc_taxi_dbt/models/marts/daily_weather_impact.sql
+++ nyc_taxi_dbt/models/marts/daily_weather_impact.sql
@@ -1,13 +1,13 @@
 {{ config(materialized='table') }}
 
 SELECT
-    date AS pickup_date,
-    borough AS pickup_borough,
-    total_revenue,
-    avg_precipitation AS avg_precip,
+    pickup_date,
+    pickup_borough,
+    daily_revenue AS total_revenue,
+    avg_precip,
     CASE
-        WHEN avg_precipitation > 0.5 THEN 'HIGH'
-        WHEN avg_precipitation > 0.1 THEN 'MEDIUM'
+        WHEN avg_precip > 0.5 THEN 'HIGH'
+        WHEN avg_precip > 0.1 THEN 'MEDIUM'
         ELSE 'LOW'
     END AS precipitation_severity
 FROM {{ source('pipeline_output', 'daily_aggregations') }}

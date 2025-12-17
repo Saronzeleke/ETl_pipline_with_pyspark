7c35d2377c04f2d96de57c075124020d152d1d1a
Saronzeleke <Cassiopiasaron@gmail.com>
2025-12-17 13:22:10 +0300

    dbt_project_model

--- dbt_project/models/marts/final_trips_summary.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    pickup_date,
-    PULocationID AS location_id,
-    trip_count,
-    avg_fare,
-    daily_revenue
-FROM daily_aggregations
-WHERE trip_count > 0
-ORDER BY pickup_date DESC
